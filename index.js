8ddd698b9afe32b9e21327b2de46a21d9b8bc41e
Todd Wolfson <todd@twolfson.com>
2014-05-02 23:41:15 -0700

    Adding TODOs and notes to self

--- index.js
+++ index.js
@@ -11,11 +11,14 @@ var win = gui.Window.get(window.open('https://play.google.com/music'));
 // TODO: Fill out package.json in full
 // TODO: Add tray menu for all methods we want
 
+// TODO: Look into icon for taskbar item https://github.com/rogerwang/node-webkit/wiki/Icons
+
 // Set up minification
 // Modified from https://github.com/rogerwang/node-webkit/wiki/Minimize-to-tray
 
 // Create our tray icon
 // DEV: Icon via http://lorempixel.com/32/32/abstract/3/
+// https://github.com/rogerwang/node-webkit/wiki/Tray
 var tray = new gui.Tray({icon: 'icon.png'});
 
 // When the tray icon is clicked, show the window
