839358358456b260b3ae3a0953b8d3829b70082c
Todd Wolfson <todd@twolfson.com>
2014-05-30 01:33:04 -0700

    Grabbing Sublime Text gnome setup from dotfiles and modifying for Google Music Webkit

---
+++ install-gnome.sh
@@ -0,0 +1,14 @@
+sudo ln -s "$PWD" /usr/lib/google-music-webkit
+
+<<EOF
+[Desktop Entry]
+Version=1.0
+Type=Application
+Terminal=false
+Exec=nw /usr/local/lib/google-music-webkit/index.js
+Icon[en_US]=/usr/local/lib/google-music-webkit/lib/icon.png
+Name[en_US]=Google Music Webkit
+Icon=/usr/local/lib/google-music-webkit/lib/icon.png
+Name=Google Music Webkit
+Comment=Open Google Music Webkit
+EOF
